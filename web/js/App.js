91b33f57ef41f8bb25e6f1cce25dbde1a101ad72
Tympanix <tympanix@gmail.com>
2017-12-28 21:24:17 +0100

    Added web source files

---
+++ web/js/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class App extends Component {
+  render() {
+    return (
+      // Add your component markup and other subcomponent references here.
+      <h1>Hello, World!</h1>
+    );
+  }
+}
