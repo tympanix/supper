4166e91608672165e0d12cfd98f06fe1890c49a4
Tympanix <tympanix@gmail.com>
2018-01-21 23:19:53 +0100

    Add production build webpack configuration

---
+++ webpack.dev.js
@@ -0,0 +1,20 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: 'web',
+    inline: true,
+    host: "0.0.0.0",
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5670',
+        secure: false
+      }
+    },
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  },
+});
