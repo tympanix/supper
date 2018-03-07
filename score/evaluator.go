193990ac9b3cfc1f0cf7f736a647cc6269918242
tympanix <tympanix@gmail.com>
2018-03-07 23:59:27 +0100

    Improved evaluator

--- score/evaluator.go
+++ score/evaluator.go
@@ -3,8 +3,17 @@ package score
 import (
 	"math"
 
+	"github.com/tympanix/supper/meta/codec"
+	"github.com/tympanix/supper/meta/quality"
+	"github.com/tympanix/supper/meta/source"
 	"github.com/tympanix/supper/types"
 	"github.com/xrash/smetrics"
+)
+
+const (
+	qualityWeight = 0.50
+	sourceWeight  = 0.75
+	codecWeight   = 0.25
 )
 
 // DefaultEvaluator uses string similarity to rate subtitles against media files
@@ -32,17 +41,19 @@ func (e *DefaultEvaluator) Evaluate(f types.LocalMedia, s types.Subtitle) float3
 
 // EvaluateMovie returns the matching score for a movie
 func (e *DefaultEvaluator) EvaluateMovie(media types.Movie, sub types.Movie) float32 {
+	if media.Year() > 0 && sub.Year() > 0 && media.Year() != sub.Year() {
+		return 0.0
+	}
+
 	prob := NewWeighted()
 	score := smetrics.JaroWinkler(media.MovieName(), sub.MovieName(), 0.7, 4)
 	tags := math.Min(float64(len(sub.AllTags())/len(media.AllTags())), 1)
 
 	prob.AddScore(score, 3)
 	prob.AddScore(tags, 2)
-	prob.AddEquals(media.Group(), sub.Group(), 1)
+
+	e.evaluateMetadata(prob, media, sub)
 	prob.AddEquals(media.Year(), sub.Year(), 1)
-	prob.AddEquals(media.Quality(), sub.Quality(), 0.5)
-	prob.AddEquals(media.Source(), sub.Source(), 0.75)
-	prob.AddEquals(media.Codec(), sub.Codec(), 0.25)
 
 	return float32(prob.Score())
 }
@@ -55,10 +66,38 @@ func (e *DefaultEvaluator) EvaluateEpisode(media types.Episode, sub types.Episod
 	show := smetrics.JaroWinkler(media.TVShow(), sub.TVShow(), 0.0, 1)
 
 	prob.AddScore(show, 1)
-	prob.AddEquals(media.Group(), sub.Group(), 1)
-	prob.AddEquals(media.Quality(), sub.Quality(), 0.5)
-	prob.AddEquals(media.Source(), sub.Source(), 0.75)
-	prob.AddEquals(media.Codec(), sub.Codec(), 0.25)
+	e.evaluateMetadata(prob, media, sub)
 
 	return float32(prob.Score())
+}
+
+func (e *DefaultEvaluator) evaluateMetadata(p *Weighted, media types.Metadata, sub types.Metadata) {
+	p.AddEquals(media.Group(), sub.Group(), 1)
+
+	if media.Quality() != quality.None && sub.Quality() != quality.None {
+		if media.Quality() == sub.Quality() {
+			p.AddScore(1.0, qualityWeight)
+		} else {
+			diff := math.Abs(float64(media.Quality() - sub.Quality()))
+			p.AddScore(0.0, diff*qualityWeight)
+		}
+	}
+
+	if media.Source() != source.None && sub.Source() != source.None {
+		if media.Source() == sub.Source() {
+			p.AddScore(1.0, sourceWeight)
+		} else {
+			diff := math.Abs(float64(media.Source() - sub.Source()))
+			p.AddScore(0.0, diff*sourceWeight)
+		}
+	}
+
+	if media.Codec() != codec.None && sub.Codec() != codec.None {
+		if media.Codec() == sub.Codec() {
+			p.AddScore(1.0, codecWeight)
+		} else {
+			diff := math.Abs(float64(media.Codec() - sub.Codec()))
+			p.AddScore(1.0, diff*codecWeight)
+		}
+	}
 }
