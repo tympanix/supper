4bab928e89ad8895e46af91f75ea4c068d6bc3b9
tympanix <tympanix@gmail.com>
2018-03-08 14:29:39 +0100

    IMproved source regex matching

--- parse/source.go
+++ parse/source.go
@@ -3,19 +3,19 @@ package parse
 import "github.com/tympanix/supper/meta/source"
 
 var sourceMap = map[string]interface{}{
-	"CAMRip|CAM":                                             source.Cam,
-	"TS|HDTS|TELESYNC|PDVD":                                  source.Telesync,
-	"WP|WORKPRINT":                                           source.Workprint,
-	"TC|HDTC|TELECINE":                                       source.Telecine,
-	"SCR|SCREENER|DVDSCR|DVDSCREENER|DBSCR":                  source.Screener,
-	"R5|R5.LINE":                                             source.R5,
-	"DVD-?Rip|DVDMux":                                        source.DVDRip,
-	"DVD-?(R|5|9)|DVD-?Full|ISO":                             source.DVDR,
-	"DSR|DSRip|SATRip|DTHRip|DVBRip|HDTV|PDTV|TVRip|HDTVRip": source.HDTV,
-	"VOD-?Rip|VODR":                                          source.VODRip,
-	"WEB.?DL(Rip)?|HDRip":                                    source.WEBDL,
-	"WEB.?Rip":                                               source.WEBRip,
-	"Blu.?Ray|BDRip|BRRip|BDMV|BDR|BD(5|9)":                  source.BluRay,
+	"CAM.?Rip|CAM|HD.?CAM":                      source.Cam,
+	"TS|HD.?TS|TELESYNC|PDVD":                   source.Telesync,
+	"WP|WORKPRINT":                              source.Workprint,
+	"TC|HD.?TC|TELECINE":                        source.Telecine,
+	"SCR|SCREENER|DVD.?SCR|DVD.?SCREENER|DBSCR": source.Screener,
+	"R5|R5.LINE":                                source.R5,
+	"DVD.?Rip|DVD.?Mux":                         source.DVDRip,
+	"DVD.?(R|5|9)(.?Full)?|ISO":                 source.DVDR,
+	"(DSR|DS|SAT|DTH|DVB|TV|HDTV|PDTV)(-?Rip)?": source.HDTV,
+	"VOD-?Rip|VODR":                             source.VODRip,
+	"WEB.?DL(Rip)?|HD.?Rip|WEB":                 source.WEBDL,
+	"WEB.?Rip":                                  source.WEBRip,
+	"Blu.?Ray|(BD|BR).?Rip|BD.?(R|5|9)":         source.BluRay,
 }
 
 // Sources lists all prossible sources to parse
