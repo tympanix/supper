88243c0128e91e40136e1e96f1f8a9484e11bd93
tympanix <tympanix@gmail.com>
2018-04-29 00:53:41 +0200

    fix: no args required for version command

--- cmd/version.go
+++ cmd/version.go
@@ -12,11 +12,14 @@ func init() {
 }
 
 var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version number and exit",
-	Args:  cobra.NoArgs,
-	Run:   showVersion,
+	Use:              "version",
+	Short:            "Print the version number and exit",
+	PersistentPreRun: noValidation,
+	Args:             cobra.NoArgs,
+	Run:              showVersion,
 }
+
+func noValidation(cmd *cobra.Command, args []string) {}
 
 func showVersion(cmd *cobra.Command, args []string) {
 	log.WithField("version", AppVersion()).
