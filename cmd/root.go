d7853dfd0614d495c163e91d3eef69ea8558c42c
tympanix <tympanix@gmail.com>
2018-04-29 00:40:08 +0200

    feat: subtitle and rename subcommands

--- cmd/root.go
+++ cmd/root.go
@@ -3,20 +3,18 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/apex/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/tympanix/supper/app"
 	"github.com/tympanix/supper/cfg"
 	"github.com/tympanix/supper/logutil"
 )
 
 var (
 	appName    = "Supper"
-	appDesc    = "Download subtitles in a breeze"
+	appDesc    = "A blazingly fast multimedia manager"
 	appVersion = "master"  // set with ldflags -X
 	appCommit  = "HEAD"    // set with ldflags -X
 	appDate    = "unknown" // set with ldflags -X
@@ -52,7 +50,6 @@ var rootCmd = &cobra.Command{
 	Short:            AppDesc(),
 	PersistentPreRun: validateFlags,
 	Args:             validateArgs,
-	Run:              downloadSubtitles,
 }
 
 // Execute executes the CLI application
@@ -68,31 +65,19 @@ func init() {
 	flags := rootCmd.PersistentFlags()
 
 	// Set up cobra command line flags
-	flags.StringSliceP("lang", "l", []string{}, "download subtitle in specified language")
-	flags.BoolP("impaired", "i", false, "hearing impaired subtitles only")
-	flags.Int("limit", 12, "limit maximum number of media to process")
-	flags.StringP("modified", "m", "", "only process media modified within specified duration")
-	flags.Bool("dry", false, "scan media but do not download any subtitles")
-	flags.IntP("score", "s", 0, "only download subtitles ranking higher than specified percent")
-	flags.String("delay", "", "wait specified duration before downloading next subtitle")
-	flags.Bool("force", false, "overwrite existing subtitles")
+	flags.Bool("dry", false, "test run command without any effects")
+	flags.Bool("force", false, "overwrite media files on conflicts")
 	flags.String("config", "", "load config file at specified path")
 	flags.String("logfile", "", "store application logs in specified path")
 	flags.BoolP("verbose", "v", false, "enable verbose logging")
-	flags.Bool("strict", false, "exit the application on any error instead of proceeding to next media item")
+	flags.Bool("strict", false, "exit the application on any error")
 	flags.Bool("version", false, "show the application version and exit")
 
 	// Set up aliases
 	viper.RegisterAlias("lang", "languages")
 
 	// Bind flags to viper
-	viper.BindPFlag("languages", flags.Lookup("lang"))
-	viper.BindPFlag("impaired", flags.Lookup("impaired"))
-	viper.BindPFlag("limit", flags.Lookup("limit"))
-	viper.BindPFlag("modified", flags.Lookup("modified"))
 	viper.BindPFlag("dry", flags.Lookup("dry"))
-	viper.BindPFlag("score", flags.Lookup("score"))
-	viper.BindPFlag("delay", flags.Lookup("delay"))
 	viper.BindPFlag("force", flags.Lookup("force"))
 	viper.BindPFlag("config", flags.Lookup("config"))
 	viper.BindPFlag("logfile", flags.Lookup("logfile"))
@@ -162,14 +147,7 @@ func readConfigFiles() {
 }
 
 func validateFlags(cmd *cobra.Command, args []string) {
-	if cfg.Default.Languages().Size() == 0 {
-		log.Fatal("Missing language flag(s)")
-	}
 
-	// Make sure score is between 0 and 100
-	if cfg.Default.Score() < 0 || cfg.Default.Score() > 100 {
-		log.WithField("score", cfg.Default.Score()).Fatalf("Score must be between 0 and 100")
-	}
 }
 
 func validateArgs(cmd *cobra.Command, args []string) error {
@@ -191,42 +169,4 @@ func validateArgs(cmd *cobra.Command, args []string) error {
 	}
 
 	return nil
-}
-
-func downloadSubtitles(cmd *cobra.Command, args []string) {
-	// Create new application
-	app := app.NewFromDefault()
-	config := app.Config()
-
-	// Search all argument paths for media
-	media, err := app.FindMedia(args...)
-
-	if err != nil {
-		log.WithError(err).Fatal("Online search failed")
-	}
-
-	if config.Modified() > 0 {
-		media = media.FilterModified(config.Modified())
-	}
-
-	if media.Len() > config.Limit() && !config.Dry() && config.Limit() != -1 {
-		log.WithFields(log.Fields{
-			"media": strconv.Itoa(media.Len()),
-			"limit": strconv.Itoa(config.Limit()),
-		}).Fatal("Media limit exceeded")
-	}
-
-	numsubs, err := app.DownloadSubtitles(media, config.Languages())
-
-	if err != nil {
-		log.WithError(err).Fatal("Download incomplete")
-	}
-
-	if config.Dry() {
-		ctx := log.WithField("reason", "dry-run")
-		ctx.Warn("Nothing performed")
-		ctx.Warnf("Media files: %v", media.Len())
-		ctx.Warnf("Missing subtitles: %v", numsubs)
-	}
-
 }
---
+++ cmd/subtitle.go
@@ -0,0 +1,87 @@
+package cmd
+
+import (
+	"strconv"
+
+	"github.com/apex/log"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/tympanix/supper/app"
+	"github.com/tympanix/supper/cfg"
+)
+
+func init() {
+	flags := subtitleCmd.Flags()
+
+	flags.IntP("score", "s", 0, "only download subtitles ranking higher than specified percent")
+	flags.String("delay", "", "wait specified duration before downloading next subtitle")
+	flags.StringSliceP("lang", "l", []string{}, "download subtitle in specified language")
+	flags.BoolP("impaired", "i", false, "hearing impaired subtitles only")
+	flags.Int("limit", 12, "limit maximum number of media to process")
+	flags.StringP("modified", "m", "", "only process media modified within specified duration")
+
+	viper.BindPFlag("languages", flags.Lookup("lang"))
+	viper.BindPFlag("impaired", flags.Lookup("impaired"))
+	viper.BindPFlag("limit", flags.Lookup("limit"))
+	viper.BindPFlag("modified", flags.Lookup("modified"))
+	viper.BindPFlag("score", flags.Lookup("score"))
+	viper.BindPFlag("delay", flags.Lookup("delay"))
+
+	rootCmd.AddCommand(subtitleCmd)
+}
+
+var subtitleCmd = &cobra.Command{
+	Use:     "subtitle",
+	Short:   "Download subtitles for media",
+	Aliases: []string{"sub"},
+	PreRun:  validateSubtitleFlags,
+	Run:     downloadSubtitles,
+}
+
+func validateSubtitleFlags(cmd *cobra.Command, args []string) {
+	if cfg.Default.Languages().Size() == 0 {
+		log.Fatal("Missing language flag(s)")
+	}
+
+	// Make sure score is between 0 and 100
+	if cfg.Default.Score() < 0 || cfg.Default.Score() > 100 {
+		log.WithField("score", cfg.Default.Score()).Fatalf("Score must be between 0 and 100")
+	}
+}
+
+func downloadSubtitles(cmd *cobra.Command, args []string) {
+	// Create new application
+	app := app.NewFromDefault()
+	config := app.Config()
+
+	// Search all argument paths for media
+	media, err := app.FindMedia(args...)
+
+	if err != nil {
+		log.WithError(err).Fatal("Online search failed")
+	}
+
+	if config.Modified() > 0 {
+		media = media.FilterModified(config.Modified())
+	}
+
+	if media.Len() > config.Limit() && !config.Dry() && config.Limit() != -1 {
+		log.WithFields(log.Fields{
+			"media": strconv.Itoa(media.Len()),
+			"limit": strconv.Itoa(config.Limit()),
+		}).Fatal("Media limit exceeded")
+	}
+
+	numsubs, err := app.DownloadSubtitles(media, config.Languages())
+
+	if err != nil {
+		log.WithError(err).Fatal("Download incomplete")
+	}
+
+	if config.Dry() {
+		ctx := log.WithField("reason", "dry-run")
+		ctx.Warn("Nothing performed")
+		ctx.Warnf("Media files: %v", media.Len())
+		ctx.Warnf("Missing subtitles: %v", numsubs)
+	}
+}
