f1e2f0289b750e2cbf51889901dd4163c3b0318a
tympanix <tympanix@gmail.com>
2018-05-06 00:11:29 +0200

    test: video interface

--- media/file.go
+++ media/file.go
@@ -3,17 +3,9 @@ package media
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
-	"path/filepath"
-	"strings"
 
-	"github.com/tympanix/supper/list"
-	"github.com/tympanix/supper/parse"
 	"github.com/tympanix/supper/types"
-	"golang.org/x/text/language"
 )
 
 // File represents a local media file on disk
@@ -34,82 +26,4 @@ func (f *File) MarshalJSON() (b []byte, err error) {
 // String returns a string representation of the media in the file
 func (f *File) String() string {
 	return f.Media.String()
-}
-
-// ExistingSubtitles returns a list of existing subtitles for the media
-func (f *File) ExistingSubtitles() (types.SubtitleList, error) {
-	folder := filepath.Dir(f.Path())
-	name := parse.Filename(f.Path())
-
-	files, err := ioutil.ReadDir(folder)
-
-	if err != nil {
-		return nil, err
-	}
-
-	subtitles := make([]types.Subtitle, 0)
-	for _, file := range files {
-		if file.IsDir() {
-			continue
-		}
-		if !strings.HasPrefix(file.Name(), name) {
-			continue
-		}
-		sub, err := NewLocalSubtitle(file)
-		if err != nil {
-			continue
-		}
-		subtitles = append(subtitles, sub)
-	}
-	return list.Subtitles(subtitles...), nil
-}
-
-// SaveSubtitle saves the subtitle for the given media to disk
-func (f *File) SaveSubtitle(s types.Downloadable, lang language.Tag) (types.LocalSubtitle, error) {
-	if s == nil {
-		return nil, errors.New("invalid subtitle nil")
-	}
-
-	srt, err := s.Download()
-
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	defer srt.Close()
-
-	name := fmt.Sprintf("%s.%s.%s", parse.Filename(f.Path()), lang, "srt")
-	folder := filepath.Dir(f.Path())
-	srtpath := filepath.Join(folder, name)
-
-	file, err := os.Create(srtpath)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer file.Close()
-	_, err = io.Copy(file, srt)
-
-	if err != nil {
-		return nil, err
-	}
-
-	info, err := file.Stat()
-	if err != nil {
-		return nil, err
-	}
-
-	sub := struct {
-		FilePath
-		os.FileInfo
-		types.Subtitle
-	}{
-		FilePath(srtpath),
-		info,
-		s.(types.Subtitle),
-	}
-
-	return sub, nil
 }
---
+++ media/video_test.go
@@ -0,0 +1,42 @@
+package media
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tympanix/supper/types"
+	"golang.org/x/text/language"
+)
+
+func TestVideo(t *testing.T) {
+	f, err := NewLocalFile("test/Inception 2010 720p.mp4")
+	require.NoError(t, err)
+
+	_, ok := f.TypeMovie()
+	assert.True(t, ok)
+
+	v, ok := f.(types.Video)
+	require.True(t, ok)
+
+	s, err := v.ExistingSubtitles()
+	require.NoError(t, err)
+
+	assert.Equal(t, 1, s.Len())
+	assert.Equal(t, language.English, s.List()[0].Language())
+}
+
+func TestNoVideo(t *testing.T) {
+	f, err := NewLocalFile("test/Inception 2010 720p.en.srt")
+	require.NoError(t, err)
+
+	_, ok := f.TypeSubtitle()
+	assert.True(t, ok)
+
+	fmt.Printf("%v", reflect.TypeOf(f))
+
+	_, ok = f.(types.Video)
+	assert.False(t, ok)
+}
