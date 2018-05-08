ff037a32bbe2f175ad5a3883ed7489988e6f8670
tympanix <tympanix@gmail.com>
2018-05-08 14:47:53 +0200

    fix: test for media containing numbers

--- parse/parse.go
+++ parse/parse.go
@@ -39,7 +39,7 @@ func Filename(filename string) string {
 }
 
 var abbreviationRegexp = regexp.MustCompile(`[A-Z](\s)[A-Z]`)
-var illegalcharsRegexp = regexp.MustCompile(`[^\p{L}\s&'_\(\)-]`)
+var illegalcharsRegexp = regexp.MustCompile(`[^\p{L}0-9\s&'_\(\)-]`)
 
 // CleanName returns the movie name cleaned from punctuation
 func CleanName(name string) string {
