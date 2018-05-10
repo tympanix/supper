6b03436e264c05ecf7eaeee93069f8733e5d147c
tympanix <tympanix@gmail.com>
2018-05-10 17:51:01 +0200

    fix: improve quality parsing

--- parse/quality.go
+++ parse/quality.go
@@ -4,11 +4,11 @@ import "github.com/tympanix/supper/meta/quality"
 
 var qualityMap = map[string]interface{}{
 	"2160p|4K|UHD|Ultra.?HD": quality.UHD2160p,
-	"QHD":   quality.QHD1440p,
-	"1080p": quality.HD1080p,
-	"720p":  quality.HD720p,
-	"576p":  quality.SD576p,
-	"480p":  quality.SD480p,
+	"1440p|QHD":              quality.QHD1440p,
+	"1080p|Full.?HD":         quality.HD1080p,
+	"720p":                   quality.HD720p,
+	"576p":                   quality.SD576p,
+	"480p":                   quality.SD480p,
 }
 
 // Qualities list all possible qualities to parse
