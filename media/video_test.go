f1d604e11d2445838db56b966f1f64e800d13154
tympanix <tympanix@gmail.com>
2018-05-14 17:53:22 +0200

    test: save subtitle

--- media/video_test.go
+++ media/video_test.go
@@ -1,6 +1,10 @@
 package media
 
 import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -35,4 +39,32 @@ func TestNoVideo(t *testing.T) {
 
 	_, ok = f.(types.Video)
 	assert.False(t, ok)
+}
+
+func TestSaveSubtitle(t *testing.T) {
+	f, err := NewLocalFile("test/Inception 2010 720p.mp4")
+	require.NoError(t, err)
+	v, ok := f.(types.Video)
+	require.True(t, ok)
+
+	sample := []byte("this is a test")
+
+	buf := bytes.NewBuffer(sample)
+	s, err := v.SaveSubtitle(buf, language.German)
+	require.NoError(t, err)
+	defer func() {
+		os.Remove(s.Path())
+	}()
+
+	assert.Equal(t, language.German, s.Language())
+	assert.True(t, strings.HasSuffix(s.Path(), ".de.srt"))
+	assert.Contains(t, s.Path(), "Inception 2010 720p")
+
+	in, err := os.Open(s.Path())
+	require.NoError(t, err)
+
+	defer in.Close()
+	data, err := ioutil.ReadAll(in)
+	require.NoError(t, err)
+	assert.Equal(t, data, sample)
 }
