11746b602be822736efcec529d88fd3539764962
tympanix <tympanix@gmail.com>
2018-05-23 20:56:37 +0200

    test: duration parsing

--- parse/duration_test.go
+++ parse/duration_test.go
@@ -13,3 +13,17 @@ func TestDuration(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, d, 2*time.Hour+3*time.Minute)
 }
+
+func TestDurationError(t *testing.T) {
+	for _, str := range []string{
+		"123",
+		"12/#m23?s",
+		"42q",
+		"_pppp",
+		"????",
+	} {
+		d, err := Duration(str)
+		assert.Error(t, err)
+		assert.Equal(t, time.Duration(0), d)
+	}
+}
