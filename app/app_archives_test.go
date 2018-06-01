4fc0b79b39114c71af8fc860dbafb3257a6152fe
tympanix <tympanix@gmail.com>
2018-06-01 14:19:50 +0200

    test: app archives

---
+++ app/app_archives_test.go
@@ -0,0 +1,79 @@
+package app
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/tympanix/supper/media"
+	"github.com/tympanix/supper/types"
+)
+
+type extractTest interface {
+	Pre(*testing.T, []types.MediaArchive)
+	Input() string
+}
+
+func TestAppArchives(t *testing.T) {
+	defer cleanRenameTest(t)
+
+	config := defaultConfig
+
+	// first run, should succeed
+	err := performExtractionTest(t, extractOkTest{}, config)
+	require.NoError(t, err)
+
+	files, err := ioutil.ReadDir("out")
+	require.NoError(t, err)
+	assert.Equal(t, len(files), 4)
+
+	// second run, should fail (no overwrite)
+	err = performExtractionTest(t, extractOkTest{}, config)
+	require.Error(t, err)
+	assert.True(t, media.IsExistsErr(err))
+}
+
+func TestAppArchivesInvalidPath(t *testing.T) {
+	config := defaultConfig
+
+	app := New(config)
+
+	_, err := app.FindArchives("deosnotexist")
+	assert.Error(t, err)
+}
+
+func performExtractionTest(t *testing.T, test extractTest, config types.Config) error {
+	app := New(config)
+
+	arch, err := app.FindArchives(test.Input())
+	require.NoError(t, err)
+
+	test.Pre(t, arch)
+
+	for _, a := range arch {
+		f, err := a.Next()
+
+		for err != io.EOF {
+			fmt.Println(f.String())
+			if err = app.ExtractMedia(f); err != nil {
+				return err
+			}
+			f, err = a.Next()
+		}
+	}
+	return nil
+}
+
+type extractOkTest struct{}
+
+func (extractOkTest) Pre(t *testing.T, a []types.MediaArchive) {
+	assert.Equal(t, len(a), 2)
+}
+
+func (extractOkTest) Input() string {
+	return "../test/archives"
+}
