c04023bab40c930d30ac0743de2c6dfa8abca4cf
tympanix <tympanix@gmail.com>
2018-06-21 00:28:49 +0200

    fix: robust h264/h265 parsing

--- parse/codec.go
+++ parse/codec.go
@@ -3,13 +3,13 @@ package parse
 import "github.com/tympanix/supper/meta/codec"
 
 var codecMap = map[string]interface{}{
-	"HEVC":    codec.HEVC,
-	"AVC":     codec.AVC,
-	"[xh]265": codec.X265,
-	"[xh]264": codec.X264,
-	"XviD":    codec.XviD,
-	"DivX":    codec.DivX,
-	"WMV":     codec.WMV,
+	"HEVC":      codec.HEVC,
+	"AVC":       codec.AVC,
+	"[xh].?265": codec.X265,
+	"[xh].?264": codec.X264,
+	"XviD":      codec.XviD,
+	"DivX":      codec.DivX,
+	"WMV":       codec.WMV,
 }
 
 // Codecs is a list of parseable codecs
